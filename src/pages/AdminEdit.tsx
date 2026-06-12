7cff995da898da309330cf2e79e57e4ffc2d348e
Mafikul Islam <mafikul.bdset@gmail.com>
2026-06-12 16:21:20 +0600

    style(admin): add dark mode support to product input

--- src/pages/AdminEdit.tsx
+++ src/pages/AdminEdit.tsx
@@ -107,7 +107,7 @@ export default function AdminEdit() {
                                         required
                                         value={name}
                                         onChange={(e) => setName(e.target.value)}
-                                        className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"
+                                        className="block w-full rounded-md border-0 py-2 px-3 text-gray-900 dark:text-white dark:bg-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black dark:focus:ring-white sm:text-sm sm:leading-6"
                                         placeholder="E.g., Minimalist Desk Lamp"
                                     />
                                 </div>
